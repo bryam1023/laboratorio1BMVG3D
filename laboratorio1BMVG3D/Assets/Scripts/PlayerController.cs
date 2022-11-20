02f9c63799390ea00a53e6db6c3f43968d60bfec
bryam1023 <bryvelasquezg@upt.pe>
2022-11-19 20:39:49 -0500

    UWU

---
+++ laboratorio1BMVG3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private RaycastHit clickInfo = new RaycastHit();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out clickInfo))
+                agent.destination = clickInfo.point;
+        }
+
+        if (agent.isOnOffMeshLink)
+        {
+            agent.CompleteOffMeshLink();
+        }
+
+
+    }
+}
